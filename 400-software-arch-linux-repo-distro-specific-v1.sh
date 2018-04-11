4d6d1777bf4293a6c6190288044b4d7fcb1e55a3
Erik Dubois <erik.dubois@gmail.com>
2018-04-11 16:57:24 +0200

    first commit made on Arch Linux! - check on ArcoLinux

---
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+# software from AUR (Arch User Repositories)
+# https://aur.archlinux.org/packages/
+
+echo "DESKTOP SPECIFIC APPLICATIONS"
+
+echo "Installing category Accessories"
+
+echo "Installing category Development"
+
+echo "Installing category Education"
+
+echo "Installing category Games"
+
+echo "Installing category Graphics"
+
+echo "Installing category Internet"
+
+echo "Installing category Multimedia"
+
+sh AUR/install-mpd-ncmpcpp-v*.sh
+
+echo "Installing category Office"
+
+echo "Installing category Other"
+
+echo "Installing category System"
+
+
+echo "################################################################"
+echo "####    Software from Arch Linux Repository installed     ######"
+echo "################################################################"
---
+++ Personal/200-install-personal-bookmarks-v1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Erik Dubois
+# Website : https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+echo "Copy bookmarks"
+
+[ -d $HOME"/.config/deepin/dde-file-manager" ] || mkdir -p $HOME"/.config/deepin/dde-file-manager"
+
+cp -r settings/bookmarks/* ~/.config/deepin/dde-file-manager
+
+echo "################################################################"
+echo "#########      bookmarks  copied                ################"
+echo "################################################################"
