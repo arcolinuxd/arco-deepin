ac20155aa07ca653c5644ad1493595ca638c79d0
erik <erik.dubois@gmail.com>
2020-04-22 18:04:57 +0200

    migration

--- 100-display-manager-and-desktop-v1.sh
+++ 100-display-manager-and-desktop-v1.sh
@@ -24,23 +24,9 @@ sudo pacman -Syyu --noconfirm
 #bug with deepin-anything - see github
 sudo pacman -S linux-headers --noconfirm --needed
 
-#installing displaymanager or login manager
-sudo pacman -S --noconfirm --needed lightdm
-sudo pacman -S --noconfirm --needed arcolinux-lightdm-gtk-greeter arcolinux-lightdm-gtk-greeter-settings
-sudo pacman -S --noconfirm --needed arcolinux-wallpapers-git 
-#installing desktop environment
-sudo pacman -S deepin deepin-extra --noconfirm --needed
-#enabling displaymanager or login manager
-sudo systemctl enable lightdm.service -f
-sudo systemctl set-default graphical.target
+
 
 echo "Changing /etc/lightdm/lightdm.conf to deepin"
 
 sudo sed -i 's/'#user-session=default'/'user-session=deepin'/g' /etc/lightdm/lightdm.conf
 sudo sed -i 's/'#greeter-session=example-gtk-gnome'/'greeter-session=lightdm-gtk-greeter'/g' /etc/lightdm/lightdm.conf
-
-#this is here if one decides to reboot after this script then we have a nice wallpaper
-sudo pacman -S arcolinux-wallpapers-git --noconfirm --needed
-#Remove anything you do not like from the installed applications
-
-#sudo pacman -R ...
