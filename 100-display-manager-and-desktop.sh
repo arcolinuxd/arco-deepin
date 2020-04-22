e2eb503cbd71f79a6866aa8ebc3fef721b51b0eb
erik <erik.dubois@gmail.com>
2020-04-22 18:10:50 +0200

    network manager in 100

--- 100-display-manager-and-desktop.sh
+++ 100-display-manager-and-desktop.sh
@@ -82,6 +82,12 @@ echo "################################################################"
 echo;tput sgr0
 sudo systemctl enable lightdm.service -f
 
+tput setaf 2;echo "################################################################"
+echo "Removing packages we do not want"
+echo "################################################################"
+echo;tput sgr0
+sudo pacman -R networkmanager --noconfirm
+
 tput setaf 7;echo "################################################################"
 echo "You now have a very minimal functional desktop"
 echo "################################################################"
